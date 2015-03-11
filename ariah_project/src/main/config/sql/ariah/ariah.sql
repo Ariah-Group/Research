910a5d668aeeeefaf390bc80368b5837891f5145
Ariah Group <code@ariahgroup.org>
2015-03-11 13:32:03 -0400

    Ability to Specify A Default Performing Organization on an IRB Protocol using a Parameter, rather than a hard-coded value of 000001

---
+++ ariah_project/src/main/config/sql/ariah/ariah.sql
@@ -0,0 +1,21 @@
+--Copyright 2015 The Ariah Group, Inc.
+--
+--Licensed under the Apache License, Version 2.0 (the "License");
+--you may not use this file except in compliance with the License.
+--You may obtain a copy of the License at
+--
+--    http://www.apache.org/licenses/LICENSE-2.0
+--
+--Unless required by applicable law or agreed to in writing, software
+--distributed under the License is distributed on an "AS IS" BASIS,
+--WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--See the License for the specific language governing permissions and
+--limitations under the License.
+
+
+-- Parameters for KRCR_PARM_T table
+INSERT INTO krcr_parm_t (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
+VALUES  ('KC-PROTOCOL','Document','ARIAH_IRB_DEFAULT_PERFORMING_ORG_ID', sys_guid(),0,'CONFG','000001','Default value for Performing Org ID for an IRB Protocol','A','KUALI');
+
+
+
--- ariah_project/src/main/java/org/kuali/kra/irb/protocol/location/ProtocolLocationServiceImpl.java
+++ ariah_project/src/main/java/org/kuali/kra/irb/protocol/location/ProtocolLocationServiceImpl.java
@@ -12,19 +12,44 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ------------------------------------------------------
+ * Updates made after January 1, 2015 are :
+ * Copyright 2015 The Ariah Group, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.kuali.kra.irb.protocol.location;
 
 import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.protocol.protocol.location.ProtocolLocationBase;
 import org.kuali.kra.protocol.protocol.location.ProtocolLocationServiceImplBase;
-
+import org.kuali.rice.coreservice.framework.parameter.ParameterService;
 
 public class ProtocolLocationServiceImpl extends ProtocolLocationServiceImplBase implements ProtocolLocationService {
 
+    /**
+     * Retrieve the Organization ID of the default Performing Organization that
+     * is set during creation of a Protocol.
+     *
+     */
     @Override
     protected String getDefaultProtocolOrganizationIdHook() {
-        return Constants.DEFAULT_PROTOCOL_ORGANIZATION_ID;
+        String defaultOrganizationId = KraServiceLocator.getService(ParameterService.class).getParameterValueAsString(Constants.MODULE_NAMESPACE_PROTOCOL,
+                Constants.PARAMETER_COMPONENT_DOCUMENT, Constants.ARIAH_PARAM_IRB_DEFAULT_PERFORMING_ORG_ID);
+
+        return defaultOrganizationId;
     }
 
     @Override
@@ -36,5 +61,4 @@ public class ProtocolLocationServiceImpl extends ProtocolLocationServiceImplBase
     protected ProtocolLocationBase getNewProtocolLocationInstanceHook() {
         return new ProtocolLocation();
     }
-    
 }
