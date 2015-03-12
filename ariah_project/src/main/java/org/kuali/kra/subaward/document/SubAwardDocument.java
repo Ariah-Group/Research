5154a41e1d54d5395004b2ef4b625c95ad5ff0f0
Ariah Group <code@ariahgroup.org>
2015-03-12 14:56:25 -0400

    reformat only.

--- ariah_project/src/main/java/org/kuali/kra/subaward/document/SubAwardDocument.java
+++ ariah_project/src/main/java/org/kuali/kra/subaward/document/SubAwardDocument.java
@@ -42,45 +42,54 @@ import org.kuali.rice.krms.api.engine.Facts.Builder;
  * This class is for subAwardDocument...
  */
 public class SubAwardDocument extends ResearchDocumentBase
-implements  Copyable, SessionDocument, KrmsRulesContext {
+        implements Copyable, SessionDocument, KrmsRulesContext {
 
-    /**.
+    /**
+     * .
      * Comment for <code>serialVersionUID</code>
      */
     private static final long serialVersionUID = 5454534590787613256L;
     private transient boolean documentSaveAfterVersioning;
     private List<SubAward> subAwardList;
     public static final String DOCUMENT_TYPE_CODE = "SAWD";
+
     @Override
     public String getDocumentTypeCode() {
         return DOCUMENT_TYPE_CODE;
     }
-    /**.
+
+    /**
+     * .
      * Constructs a subAwardDocument object
      */
     public SubAwardDocument() {
         super();
         init();
     }
+
     public SubAward getSubAward() {
         return getSubAwardList().size() > 0 ? getSubAwardList().get(0) : new SubAward();
     }
-    public void setSubAward(SubAward subAward){
+
+    public void setSubAward(SubAward subAward) {
         subAwardList.set(0, subAward);
     }
+
     public void setSubAwardList(List<SubAward> subAwardList) {
         this.subAwardList = subAwardList;
     }
+
     public List<SubAward> getSubAwardList() {
         return subAwardList;
     }
 
-    /**.
+    /**
+     * .
      * The method is for doRouteStatusChange
      */
     @Override
     public void doRouteStatusChange(
-    DocumentRouteStatusChange statusChangeEvent) {
+            DocumentRouteStatusChange statusChangeEvent) {
         super.doRouteStatusChange(statusChangeEvent);
 
         String newStatus = statusChangeEvent.getNewRouteStatus();
@@ -90,9 +99,9 @@ implements  Copyable, SessionDocument, KrmsRulesContext {
                     getUserSession().getPrincipalName());
         }
         if (newStatus.equalsIgnoreCase(KewApiConstants.ROUTE_HEADER_CANCEL_CD)
-        || newStatus.equalsIgnoreCase(
-        KewApiConstants.ROUTE_HEADER_DISAPPROVED_CD)) {
-            getVersionHistoryService().updateVersionHistory(getSubAward(), VersionStatus.CANCELED, GlobalVariables. 
+                || newStatus.equalsIgnoreCase(
+                        KewApiConstants.ROUTE_HEADER_DISAPPROVED_CD)) {
+            getVersionHistoryService().updateVersionHistory(getSubAward(), VersionStatus.CANCELED, GlobalVariables.
                     getUserSession().getPrincipalName());
         }
 
@@ -100,48 +109,51 @@ implements  Copyable, SessionDocument, KrmsRulesContext {
             subAward.setSubAwardDocument(this);
         }
     }
+
     /**
-     * This method specifies if this document may be
-     * edited; i.e. it's only initiated or saved
+     * This method specifies if this document may be edited; i.e. it's only
+     * initiated or saved
+     *
      * @return boolean
      */
     public boolean isEditable() {
-        WorkflowDocument workflowDoc =
-        getDocumentHeader().getWorkflowDocument();
+        WorkflowDocument workflowDoc
+                = getDocumentHeader().getWorkflowDocument();
         return workflowDoc.isInitiated() || workflowDoc.isSaved();
     }
-    
+
     protected void init() {
         subAwardList = new ArrayList<SubAward>();
         subAwardList.add(new SubAward());
     }
+
     /**
      * @return
      */
     public boolean isDocumentSaveAfterVersioning() {
         return documentSaveAfterVersioning;
     }
-    
+
     /**
      * @param documentSaveAfterVersioning
      */
     public void setDocumentSaveAfterSubAwardLookupEditOrVersion(boolean documentSaveAfterVersioning) {
         this.documentSaveAfterVersioning = documentSaveAfterVersioning;
     }
+
     /**
      * @return
      */
     protected VersionHistoryService getVersionHistoryService() {
         return KraServiceLocator.getService(VersionHistoryService.class);
     }
+
     /**
-     * This method is to check whether rice
-     * async routing is ok now.
-     * Close to hack.  called by holdingpageaction
-     * Different document type may have different
-     * routing set up, so each document type
-     * can implement its own
-     *  isProcessComplete
+     * This method is to check whether rice async routing is ok now. Close to
+     * hack. called by holdingpageaction Different document type may have
+     * different routing set up, so each document type can implement its own
+     * isProcessComplete
+     *
      * @return
      */
     public boolean isProcessComplete() {
@@ -150,26 +162,30 @@ implements  Copyable, SessionDocument, KrmsRulesContext {
 
         if (getDocumentHeader().hasWorkflowDocument()) {
             /**
-             * per KRACOEUS-5394 changing from getDocumentHeader().getWorkflowDocument().isFinal().  This way
-             * we route back to the award document more appropriately from holding page.
+             * per KRACOEUS-5394 changing from
+             * getDocumentHeader().getWorkflowDocument().isFinal(). This way we
+             * route back to the award document more appropriately from holding
+             * page.
              */
-            if (getDocumentHeader().getWorkflowDocument().isFinal() 
+            if (getDocumentHeader().getWorkflowDocument().isFinal()
                     || getDocumentHeader().getWorkflowDocument().isProcessed()
                     || KraServiceLocator.getService(KraWorkflowService.class).hasPendingApprovalRequests(getDocumentHeader().getWorkflowDocument())) {
                 isComplete = true;
             }
         }
-           
+
         return isComplete;
     }
+
     @Override
     public List<? extends DocumentCustomData> getDocumentCustomData() {
         return getSubAward().getSubAwardCustomDataList();
     }
-    
+
     /**
-     * 
-     * @see org.kuali.core.bo.PersistableBusinessObjectBase#buildListOfDeletionAwareLists()
+     *
+     * @see
+     * org.kuali.core.bo.PersistableBusinessObjectBase#buildListOfDeletionAwareLists()
      */
     @SuppressWarnings("unchecked")
     @Override
@@ -183,21 +199,23 @@ implements  Copyable, SessionDocument, KrmsRulesContext {
         managedLists.add(subAward.getSubAwardReportList());
         return managedLists;
     }
-    
+
     public String getDocumentBoNumber() {
         return getSubAward().getSubAwardCode();
     }
+
     @Override
     public void populateContextQualifiers(Map<String, String> qualifiers) {
         qualifiers.put("namespaceCode", Constants.MODULE_NAMESPACE_SUBAWARD);
         qualifiers.put("name", KcKrmsConstants.SubAward.SUBAWARD_CONTEXT);
     }
-    
+
     @Override
     public void addFacts(Builder factsBuilder) {
         KcKrmsFactBuilderServiceHelper fbService = KraServiceLocator.getService("subAwardFactBuilderService");
         fbService.addFacts(factsBuilder, this);
     }
+
     @Override
     public void populateAgendaQualifiers(Map<String, String> qualifiers) {
         qualifiers.put(KcKrmsConstants.UNIT_NUMBER, getLeadUnitNumber());
