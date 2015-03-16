336df74fa0b9084098d5368cb5725346fb27b209
Ariah Group <code@ariahgroup.org>
2015-03-16 16:28:25 -0400

    Fixes for Executive SUmmary field on a Development Proposal Record

--- ariah_project/src/main/java/org/ariahgroup/research/datadictionary/AttributeDefinition.java
+++ ariah_project/src/main/java/org/ariahgroup/research/datadictionary/AttributeDefinition.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2015 The Ariah Group, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.ariahgroup.research.datadictionary;
 
@@ -21,7 +21,7 @@ import org.ariahgroup.research.datadictionary.validation.constraint.WordCountCon
 /**
  * adds custom fields to attribute definition.
  *
-* @author The Ariah Group, Inc.
+ * @author The Ariah Group, Inc.
  */
 public class AttributeDefinition extends org.kuali.rice.krad.datadictionary.AttributeDefinition
         implements WordCountConstrainable {
