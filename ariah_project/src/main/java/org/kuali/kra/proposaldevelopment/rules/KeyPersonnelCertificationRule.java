95def9dbb6de06adc9e7dcbc235b4118476895de
Ariah Group <code@ariahgroup.org>
2015-03-18 11:48:54 -0400

    Fix Proposal Development Rule Validation to Allow PI and Co-I to not have Certification If the Require Certification Flag is Set to Off.

--- ariah_project/src/main/java/org/kuali/kra/proposaldevelopment/rules/KeyPersonnelCertificationRule.java
+++ ariah_project/src/main/java/org/kuali/kra/proposaldevelopment/rules/KeyPersonnelCertificationRule.java
@@ -12,6 +12,22 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ------------------------------------------------------
+ * Updates made after January 1, 2015 are :
+ * Copyright 2015 The Ariah Group, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.kuali.kra.proposaldevelopment.rules;
 
@@ -50,21 +66,21 @@ public class KeyPersonnelCertificationRule extends ResearchDocumentRuleBase impl
 
     private static final String BEFORE_APPROVE = "BA";
     private static final String BEFORE_SUBMIT = "BS";
-    
+
     private static final Log LOG = LogFactory.getLog(ProposalDevelopmentProposalAttachmentsAuditRule.class);
-    
+
     public boolean processRunAuditBusinessRules(Document document) {
         boolean valid = true;
 
-        if(getKeyPersonCertDeferralParm().equals(BEFORE_SUBMIT)) {
+        if (getKeyPersonCertDeferralParm().equals(BEFORE_SUBMIT)) {
             valid &= this.validateAllCertificationsComplete((ProposalDevelopmentDocument) document);
-        } else if(getKeyPersonCertDeferralParm().equals(BEFORE_APPROVE)) {
+        } else if (getKeyPersonCertDeferralParm().equals(BEFORE_APPROVE)) {
             //valid &= this.validateSpecificKeyPersonCertification((ProposalDevelopmentDocument) document);
         } else {
             LOG.warn("System parameter 'KEY_PERSON_CERTIFICATION_DEFERRAL' should be one of 'BA' or 'BS'.");
             return false;
         }
-        
+
         return valid;
     }
 
@@ -72,11 +88,11 @@ public class KeyPersonnelCertificationRule extends ResearchDocumentRuleBase impl
     public boolean processRouteDocument(Document document) {
         boolean isValid = true;
 
-        if(getKeyPersonCertDeferralParm().equals(BEFORE_SUBMIT)) {
+        if (getKeyPersonCertDeferralParm().equals(BEFORE_SUBMIT)) {
             //validation based on existence of ANY incomplete questionnaires.
             isValid &= this.validateAllCertificationsComplete((ProposalDevelopmentDocument) document);
         }
-        
+
         return isValid;
     }
 
@@ -84,100 +100,107 @@ public class KeyPersonnelCertificationRule extends ResearchDocumentRuleBase impl
     public boolean processApproveDocument(ApproveDocumentEvent approveEvent) {
         boolean isValid = true;
 
-        if(getKeyPersonCertDeferralParm().equals(BEFORE_APPROVE)) {
+        if (getKeyPersonCertDeferralParm().equals(BEFORE_APPROVE)) {
             //validation based on session user existing as key person and possibly aggregator.
             isValid &= this.validateKeyPersonCertification((ProposalDevelopmentDocument) approveEvent.getDocument(),
                     GlobalVariables.getUserSession().getPerson());
         }
-        
+
         return isValid;
-    }    
-    
+    }
+
     private boolean validateAllCertificationsComplete(ProposalDevelopmentDocument document) {
         boolean retval = true;
-    
+
         int count = 0;
 
         for (ProposalPerson person : document.getDevelopmentProposal().getProposalPersons()) {
             if (hasCertification(person) && !validKeyPersonCertification(person)) {
-                generateAuditError(count,person.getFullName());
+                generateAuditError(count, person.getFullName());
                 retval = false;
             }
             count++;
         }
-        
+
         return retval;
     }
-    
+
     /*
      * validates specifically the key person certification, if any, of the proposal person matching the
      * person in the user session
      */
     private boolean validateKeyPersonCertification(ProposalDevelopmentDocument document, Person user) {
         boolean retval = true;
-    
+
         int count = 0;
-        
+
         for (ProposalPerson person : document.getDevelopmentProposal().getProposalPersons()) {
-            if(StringUtils.equals(user.getPrincipalId(),person.getPersonId())
+            if (StringUtils.equals(user.getPrincipalId(), person.getPersonId())
                     && hasCertification(person) && !validKeyPersonCertification(person)) {
-                generateAuditError(count,person.getFullName());
+                generateAuditError(count, person.getFullName());
                 return false;
             }
             count++;
         }
-        
+
         return retval;
     }
-    
+
     private boolean validKeyPersonCertification(ProposalPerson person) {
         return validateYesNoQuestions(person);
     }
 
     private boolean hasCertification(ProposalPerson person) {
-        
+
         //questionnaires should continue to be answerable only to the following approvers,
         //possibly as well as other roles. i.e. Aggregator.
         ProposalPersonRole personRole = person.getRole();
-        if (personRole.getRoleCode().equals(Constants.CO_INVESTIGATOR_ROLE)
-                || personRole.getRoleCode().equals(Constants.PRINCIPAL_INVESTIGATOR_ROLE)
-                || (personRole.getRoleCode().equals(Constants.KEY_PERSON_ROLE) && StringUtils.isNotBlank(person.getOptInCertificationStatus())
-                        && person.getOptInCertificationStatus().equals("Y"))) {
-                return true;
+
+        if (personRole.getRoleCode().equals(Constants.PRINCIPAL_INVESTIGATOR_ROLE)
+                || (personRole.getRoleCode().equals(Constants.CO_INVESTIGATOR_ROLE)
+                && StringUtils.isNotBlank(person.getOptInCertificationStatus())
+                && person.getOptInCertificationStatus().equals("Y"))
+                || (personRole.getRoleCode().equals(Constants.KEY_PERSON_ROLE)
+                && StringUtils.isNotBlank(person.getOptInCertificationStatus())
+                && person.getOptInCertificationStatus().equals("Y"))) {
+            return true;
         }
-        
+
         return false;
     }
-    
+
     /**
-     * Yes/No questions have to be submitted to Grants.gov on document route. If the submitter has not completed the certifications,
-     * errors should be displayed in audit mode.<br/> 
+     * Yes/No questions have to be submitted to Grants.gov on document route. If
+     * the submitter has not completed the certifications, errors should be
+     * displayed in audit mode.<br/>
      * <br/>
-     * This method differs from <code>{@link #validateKeyPersonCertification(ProposalDevelopmentDocument)}</code> that it refers to a specific person.
-     * If any one of the Yes/No Questions is not completed, then this check will fail.
-     * 
-     * 
+     * This method differs from
+     * <code>{@link #validateKeyPersonCertification(ProposalDevelopmentDocument)}</code>
+     * that it refers to a specific person. If any one of the Yes/No Questions
+     * is not completed, then this check will fail.
+     *
+     *
      * @param investigator Proposal Investigator
      * @return true if the given PI's Yes/No Questions are completed
      */
     private boolean validateYesNoQuestions(ProposalPerson investigator) {
         boolean retval = true;
-        
+
         ProposalPersonModuleQuestionnaireBean bean = new ProposalPersonModuleQuestionnaireBean(investigator.getDevelopmentProposal(), investigator);
         List<AnswerHeader> headers = KraServiceLocator.getService(QuestionnaireAnswerService.class).getQuestionnaireAnswer(bean);
-        
+
         for (AnswerHeader head : headers) {
             retval &= head.getCompleted();
         }
-               
+
         return retval;
     }
-    
+
     private void generateAuditError(int count, String personFullName) {
         final String errorStarter = "proposalPersonQuestionnaireHelpers[";
         final String errorFinish = "].answerHeaders[0].answers[0].answer";
         final String errorLink = KEY_PERSONNEL_PAGE + "." + KEY_PERSONNEL_PANEL_ANCHOR;
-        
+
         String errorKey = errorStarter + count + errorFinish;
 
         //Displays the error within the audit log.
@@ -191,25 +214,25 @@ public class KeyPersonnelCertificationRule extends ResearchDocumentRuleBase impl
     }
 
     /**
-     * This method should only be called if an audit error is intending to be added because it will actually add a <code>{@link List<AuditError>}</code>
-     * to the auditErrorMap.
-     * 
+     * This method should only be called if an audit error is intending to be
+     * added because it will actually add a
+     * <code>{@link List<AuditError>}</code> to the auditErrorMap.
+     *
      * @return List of AuditError instances
      */
     @SuppressWarnings("unchecked")
     private List<AuditError> getAuditErrors() {
         List<AuditError> auditErrors = new ArrayList<AuditError>();
-        
+
         if (!KNSGlobalVariables.getAuditErrorMap().containsKey(KEY_PERSONNEL_AUDIT_CLUSTER_KEY)) {
-            KNSGlobalVariables.getAuditErrorMap().put(KEY_PERSONNEL_AUDIT_CLUSTER_KEY, 
+            KNSGlobalVariables.getAuditErrorMap().put(KEY_PERSONNEL_AUDIT_CLUSTER_KEY,
                     new AuditCluster(Constants.KEY_PERSONNEL_PANEL_NAME, auditErrors, Constants.AUDIT_ERRORS));
         } else {
             auditErrors = ((AuditCluster) KNSGlobalVariables.getAuditErrorMap().get(KEY_PERSONNEL_AUDIT_CLUSTER_KEY)).getAuditErrorList();
-                }
-        
-        return auditErrors;
-            }
+        }
 
+        return auditErrors;
+    }
 
     private String getKeyPersonCertDeferralParm() {
         return ProposalDevelopmentUtils
