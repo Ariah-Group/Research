ef4fa8827edbf3c056771341c2229f1d71ae56ea
Ariah Group <code@ariahgroup.org>
2015-03-20 23:12:17 -0400

    fixing legacy code typos

--- ariah_project/src/main/java/org/kuali/kra/committee/rule/event/CommitteeActionPrintCommitteeDocumentEvent.java
+++ ariah_project/src/main/java/org/kuali/kra/committee/rule/event/CommitteeActionPrintCommitteeDocumentEvent.java
@@ -12,6 +12,22 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ------------------------------------------------------
+ * Updates made after January 1, 2015 are :
+ * Copyright 2015 The Ariah Group, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.kuali.kra.committee.rule.event;
 
@@ -21,12 +37,13 @@ import org.kuali.kra.rule.BusinessRuleInterface;
 import org.kuali.rice.krad.document.Document;
 
 public class CommitteeActionPrintCommitteeDocumentEvent extends CommitteeActionsEventBase<CommitteeActionPrintCommitteeDocumentRule> {
+
     private static final String MSG = "print committee document";
-    
+
     private Boolean printRooster;
     private Boolean printFutureScheduledMeeting;
     private boolean onMeetingAction;
-    
+
     public CommitteeActionPrintCommitteeDocumentEvent(String errorPathPrefix, Document document, Boolean printRooster, Boolean printFutureScheduledMeeting) {
         super(MSG + getDocumentId(document), errorPathPrefix, document);
         setPrintRooster(printRooster);
@@ -34,9 +51,9 @@ public class CommitteeActionPrintCommitteeDocumentEvent extends CommitteeActions
         setOnMeetingAction(false);
     }
 
-    public CommitteeActionPrintCommitteeDocumentEvent(String errorPathPrefix, Document document, Boolean printRooster, Boolean printFutureScheduledMeeting, boolean onMeetingAction) {
+    public CommitteeActionPrintCommitteeDocumentEvent(String errorPathPrefix, Document document, Boolean printRoster, Boolean printFutureScheduledMeeting, boolean onMeetingAction) {
         super(MSG + getDocumentId(document), errorPathPrefix, document);
-        setPrintRooster(printRooster);
+        setPrintRooster(printRoster);
         setPrintFutureScheduledMeeting(printFutureScheduledMeeting);
         setOnMeetingAction(onMeetingAction);
     }
