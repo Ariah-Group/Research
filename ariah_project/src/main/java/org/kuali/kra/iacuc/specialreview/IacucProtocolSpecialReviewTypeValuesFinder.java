5f9189f377ded2a559b05f92f7e22f3daf6dd572
Ariah Group <code@ariahgroup.org>
2015-03-22 00:45:17 -0400

    Fix legacy code bug : IACUC - Creating a New IACUC Special Review Type and it does not appear in the matching drop down on the IACUC Protocol

--- ariah_project/src/main/java/org/kuali/kra/iacuc/specialreview/IacucProtocolSpecialReviewTypeValuesFinder.java
+++ ariah_project/src/main/java/org/kuali/kra/iacuc/specialreview/IacucProtocolSpecialReviewTypeValuesFinder.java
@@ -12,6 +12,22 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ------------------------------------------------------
+ * Updates made after January 1, 2015 are :
+ * Copyright 2015 The Ariah Group, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.kuali.kra.iacuc.specialreview;
 
@@ -19,13 +35,13 @@ import org.kuali.kra.bo.CoeusModule;
 import org.kuali.kra.common.specialreview.lookup.keyvalue.SpecialReviewTypeValuesFinder;
 
 /**
- * Provides a value finder for the Protocol-specific Special Review Types.
+ * Provides a value finder for the IACUC Protocol-specific Special Review Types.
  */
 public class IacucProtocolSpecialReviewTypeValuesFinder extends SpecialReviewTypeValuesFinder {
 
     @Override
     public String getModuleCode() {
-        return CoeusModule.IRB_MODULE_CODE;
+        return CoeusModule.IACUC_PROTOCOL_MODULE_CODE;
     }
 
 }
