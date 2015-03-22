46c27f4a55b95e55d4692b14e30c14bdc177934a
Ariah Group <code@ariahgroup.org>
2015-03-21 22:54:19 -0400

    Small fixes

--- ariah_project/src/main/java/org/kuali/kra/authorizer/CreateCoiDisclosureAuthorizer.java
+++ ariah_project/src/main/java/org/kuali/kra/authorizer/CreateCoiDisclosureAuthorizer.java
@@ -12,6 +12,22 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ------------------------------------------------------
+ * Updates made after January 1, 2015 are :
+ * Copyright 2015 The Ariah Group, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.kuali.kra.authorizer;
 
@@ -25,8 +41,12 @@ public class CreateCoiDisclosureAuthorizer extends TaskAuthorizerImpl {
 
     private CoiDisclosureService coiDisclosureService;
     /**
+     * @param userId
+     * @param task
+     * @return 
      * @see org.kuali.kra.authorization.TaskAuthorizer#isAuthorized(java.lang.String, org.kuali.kra.authorization.Task)
      */
+    @Override
     public boolean isAuthorized(String userId, Task task) {
         // TODO : do we let coi admin create coi disclosure if admin is not a reporter ?
         return coiDisclosureService.isReporter() || hasUnitPermission(userId, Constants.MODULE_NAMESPACE_COIDISCLOSURE, PermissionConstants.REPORT_COI_DISCLOSURE)
