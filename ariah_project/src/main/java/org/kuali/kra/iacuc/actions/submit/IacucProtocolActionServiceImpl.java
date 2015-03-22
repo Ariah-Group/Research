cd2c9e1ed265ce28c044e35160d026dadafa1488
Ariah Group <code@ariahgroup.org>
2015-03-22 00:49:07 -0400

    IACUC - Bug in IacucProtocolActionServiceImpl.removeQuestionnaireAnswer as it References IRB Module Code Instead of IACUC Module Code

--- ariah_project/src/main/java/org/kuali/kra/iacuc/actions/submit/IacucProtocolActionServiceImpl.java
+++ ariah_project/src/main/java/org/kuali/kra/iacuc/actions/submit/IacucProtocolActionServiceImpl.java
@@ -12,6 +12,22 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ------------------------------------------------------
+ * Updates made after January 1, 2015 are :
+ * Copyright 2015 The Ariah Group, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.kuali.kra.iacuc.actions.submit;
 
@@ -34,11 +50,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 /**
- * 
- * This class is to provide the 'protocol' action pre validation and post update.
- * pre-validation include canperform and authorization check.
+ *
+ * This class is to provide the 'protocol' action pre validation and post
+ * update. pre-validation include canperform and authorization check.
  * post-update will update protocol status or submission status.
  */
 public class IacucProtocolActionServiceImpl extends ProtocolActionServiceImplBase implements IacucProtocolActionService {
@@ -49,61 +64,60 @@ public class IacucProtocolActionServiceImpl extends ProtocolActionServiceImplBas
     protected static final int PERFORMACTION_RULE = 0;
 
     protected static final int UPDATE_RULE = 1;
-    
+
     protected static final int PERMISSIONS_LEADUNIT_RULE = 2;
-    
+
     protected static final int PERMISSIONS_SUBMIT_RULE = 3;
-    
+
     protected static final int PERMISSIONS_COMMITTEEMEMBERS_RULE = 4;
-    
+
     protected static final int PERMISSIONS_SPECIAL_RULE = 5;
-    
+
     private static final String PERFORMACTION_FILE = "org/kuali/kra/iacuc/drools/rules/canPerformIacucProtocolActionRules.drl";
-    
+
     private static final String KC_IACUC = "KC-IACUC";
 
-    private static final String[] actionCodes = { 
-            IacucProtocolActionType.SUBMITTED_TO_IACUC,
-            IacucProtocolActionType.AMENDMENT_CREATED,
-            IacucProtocolActionType.CONTINUATION,
-            IacucProtocolActionType.RENEWAL_CREATED,
-            IacucProtocolActionType.CONTINUATION_AMENDMENT,
-            IacucProtocolActionType.RENEWAL_AMENDMENT,
-            IacucProtocolActionType.REQUEST_DEACTIVATE,
-            IacucProtocolActionType.REQUEST_LIFT_HOLD,
-            IacucProtocolActionType.NOTIFIED_COMMITTEE,
-            IacucProtocolActionType.ADMINISTRATIVE_CORRECTION,
-            IacucProtocolActionType.NOTIFY_IACUC,
-            IacucProtocolActionType.IACUC_WITHDRAWN,
-            IacucProtocolActionType.IACUC_ABANDON,
-            IacucProtocolActionType.ASSIGNED_TO_AGENDA,
-            IacucProtocolActionType.REMOVE_FROM_AGENDA,
-            IacucProtocolActionType.RESCHEDULED,
-            IacucProtocolActionType.TABLED,
-            IacucProtocolActionType.IACUC_APPROVED,
-            IacucProtocolActionType.RESPONSE_APPROVAL,
-            IacucProtocolActionType.IACUC_ACKNOWLEDGEMENT,
-            IacucProtocolActionType.IACUC_REVIEW_NOT_REQUIRED,
-            IacucProtocolActionType.LIFT_HOLD,
-            IacucProtocolActionType.IACUC_MINOR_REVISIONS_REQUIRED,
-            IacucProtocolActionType.RETURNED_TO_PI,
-            IacucProtocolActionType.IACUC_MAJOR_REVISIONS_REQUIRED,
-            IacucProtocolActionType.DESIGNATED_REVIEW_APPROVAL,
-            IacucProtocolActionType.IACUC_REVISIONS_REQUIRED,
-            IacucProtocolActionType.FULL_COMMITEE_REQUIRED,
-            IacucProtocolActionType.ADMINISTRATIVE_APPROVAL,
-            IacucProtocolActionType.ADMINISTRATIVELY_INCOMPLETE,
-            IacucProtocolActionType.ADMINISTRATIVELY_WITHDRAWN,
-            IacucProtocolActionType.IACUC_DISAPPROVED,
-            IacucProtocolActionType.EXPIRED,
-            IacucProtocolActionType.DEACTIVATED,
-            IacucProtocolActionType.ADMINISTRATIVELY_DEACTIVATED ,
-            IacucProtocolActionType.HOLD ,
-            IacucProtocolActionType.TERMINATED,
-            IacucProtocolActionType.SUSPENDED  };     
-    
-    
-    public String getPerformActionFileNameHook() {     
+    private static final String[] actionCodes = {
+        IacucProtocolActionType.SUBMITTED_TO_IACUC,
+        IacucProtocolActionType.AMENDMENT_CREATED,
+        IacucProtocolActionType.CONTINUATION,
+        IacucProtocolActionType.RENEWAL_CREATED,
+        IacucProtocolActionType.CONTINUATION_AMENDMENT,
+        IacucProtocolActionType.RENEWAL_AMENDMENT,
+        IacucProtocolActionType.REQUEST_DEACTIVATE,
+        IacucProtocolActionType.REQUEST_LIFT_HOLD,
+        IacucProtocolActionType.NOTIFIED_COMMITTEE,
+        IacucProtocolActionType.ADMINISTRATIVE_CORRECTION,
+        IacucProtocolActionType.NOTIFY_IACUC,
+        IacucProtocolActionType.IACUC_WITHDRAWN,
+        IacucProtocolActionType.IACUC_ABANDON,
+        IacucProtocolActionType.ASSIGNED_TO_AGENDA,
+        IacucProtocolActionType.REMOVE_FROM_AGENDA,
+        IacucProtocolActionType.RESCHEDULED,
+        IacucProtocolActionType.TABLED,
+        IacucProtocolActionType.IACUC_APPROVED,
+        IacucProtocolActionType.RESPONSE_APPROVAL,
+        IacucProtocolActionType.IACUC_ACKNOWLEDGEMENT,
+        IacucProtocolActionType.IACUC_REVIEW_NOT_REQUIRED,
+        IacucProtocolActionType.LIFT_HOLD,
+        IacucProtocolActionType.IACUC_MINOR_REVISIONS_REQUIRED,
+        IacucProtocolActionType.RETURNED_TO_PI,
+        IacucProtocolActionType.IACUC_MAJOR_REVISIONS_REQUIRED,
+        IacucProtocolActionType.DESIGNATED_REVIEW_APPROVAL,
+        IacucProtocolActionType.IACUC_REVISIONS_REQUIRED,
+        IacucProtocolActionType.FULL_COMMITEE_REQUIRED,
+        IacucProtocolActionType.ADMINISTRATIVE_APPROVAL,
+        IacucProtocolActionType.ADMINISTRATIVELY_INCOMPLETE,
+        IacucProtocolActionType.ADMINISTRATIVELY_WITHDRAWN,
+        IacucProtocolActionType.IACUC_DISAPPROVED,
+        IacucProtocolActionType.EXPIRED,
+        IacucProtocolActionType.DEACTIVATED,
+        IacucProtocolActionType.ADMINISTRATIVELY_DEACTIVATED,
+        IacucProtocolActionType.HOLD,
+        IacucProtocolActionType.TERMINATED,
+        IacucProtocolActionType.SUSPENDED};
+
+    public String getPerformActionFileNameHook() {
         return PERFORMACTION_FILE;
     }
 
@@ -142,28 +156,26 @@ public class IacucProtocolActionServiceImpl extends ProtocolActionServiceImplBas
             fieldValues.put("protocolNumber", protocol.getProtocolNumber());
             businessObjectService.deleteMatching(IacucProtocolSubmissionDoc.class, fieldValues);
             removeQuestionnaireAnswer(protocolAction, protocol);
-            protocol.getProtocolSubmissions().remove(protocolAction.getProtocolSubmission()); 
-            protocol.setProtocolSubmission(null); 
+            protocol.getProtocolSubmissions().remove(protocolAction.getProtocolSubmission());
+            protocol.setProtocolSubmission(null);
         }
     }
-    
+
     /*
      * This is to remove the questionnaire answered for request submission
      */
     private void removeQuestionnaireAnswer(ProtocolActionBase protocolActionBo, ProtocolBase protocol) {
         // 'bos.deletematching' will not work because it is not deleting 'answers'
         Map<String, String> fieldValues = new HashMap<String, String>();
-        fieldValues.put("moduleItemCode", CoeusModule.IRB_MODULE_CODE);
+        fieldValues.put("moduleItemCode", CoeusModule.IACUC_PROTOCOL_MODULE_CODE);
         fieldValues.put("moduleItemKey", protocol.getProtocolNumber());
         fieldValues.put("moduleSubItemCode", CoeusSubModule.PROTOCOL_SUBMISSION);
         fieldValues.put("moduleSubItemKey", protocolActionBo.getProtocolSubmission().getSubmissionNumber().toString());
-        List<AnswerHeader> answerHeaders = (List<AnswerHeader>)businessObjectService.findMatching(AnswerHeader.class, fieldValues);
+        List<AnswerHeader> answerHeaders = (List<AnswerHeader>) businessObjectService.findMatching(AnswerHeader.class, fieldValues);
         if (!answerHeaders.isEmpty()) {
             businessObjectService.delete(answerHeaders);
         }
-    }  
-    
-    
+    }
 
     @Override
     protected Log getLOGHook() {
@@ -197,7 +209,5 @@ public class IacucProtocolActionServiceImpl extends ProtocolActionServiceImplBas
     protected String[] getActionCodesArrayHook() {
         return this.actionCodes;
     }
- 
-    
-    
+
 }
