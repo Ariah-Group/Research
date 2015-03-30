b67e302c41c5affc01a8c7f87b6b4e03d2e49fa7
Ariah Group <code@ariahgroup.org>
2015-03-30 15:09:05 -0400

    re-work config file naming to better reflect Ariah Research (AR) instead of just Ariah

--- ariah_project/src/test/java/org/kuali/kra/award/contacts/ContactRoleFixtureFactory.java
+++ ariah_project/src/test/java/org/kuali/kra/award/contacts/ContactRoleFixtureFactory.java
@@ -12,13 +12,26 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ------------------------------------------------------
+ * Updates made after January 1, 2015 are :
+ * Copyright 2015 The Ariah Group, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.kuali.kra.award.contacts;
 
 import org.kuali.kra.award.home.ContactRole;
-import org.kuali.kra.award.home.ContactType;
-import org.kuali.kra.bo.UnitAdministratorType;
-import org.kuali.kra.bo.UnitContactType;
 import org.kuali.kra.proposaldevelopment.bo.ProposalPersonRole;
 
 /**
@@ -40,18 +53,18 @@ public final class ContactRoleFixtureFactory {
         return role;
     }
     
-    private static ContactRole getUnitAdministratorType(String code, String description, UnitContactType unitContactType) {
-        UnitAdministratorType type = new UnitAdministratorType();
-        type.setUnitAdministratorTypeCode(code);
-        type.setDescription(description);
-        type.setUnitContactType(unitContactType);
-        return type;
-    }
+//    private static ContactRole getUnitAdministratorType(String code, String description, UnitContactType unitContactType) {
+//        UnitAdministratorType type = new UnitAdministratorType();
+//        type.setUnitAdministratorTypeCode(code);
+//        type.setDescription(description);
+//        type.setUnitContactType(unitContactType);
+//        return type;
+//    }
     
-    private static ContactRole getContactType(String code, String description) {
-        ContactType role = new ContactType();
-        role.setContactTypeCode(code);
-        role.setDescription(description);
-        return role;
-    }
+//    private static ContactRole getContactType(String code, String description) {
+//        ContactType role = new ContactType();
+//        role.setContactTypeCode(code);
+//        role.setDescription(description);
+//        return role;
+//    }
 }
