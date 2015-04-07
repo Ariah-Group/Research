7c05414ca8221a8a89a23060e6827121151308e8
Ariah Group <code@ariahgroup.org>
2015-04-07 15:21:06 -0400

    Implement RES-304, Subaward - SubAwardForms toString() should exclude attachmentContent

--- ar_webapp/src/main/java/org/kuali/kra/subaward/bo/SubAwardForms.java
+++ ar_webapp/src/main/java/org/kuali/kra/subaward/bo/SubAwardForms.java
@@ -31,21 +31,20 @@
  */
 package org.kuali.kra.subaward.bo;
 
-
 import org.apache.struts.upload.FormFile;
 import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
 import org.kuali.rice.krad.bo.PersistableAttachment;
 
-public class SubAwardForms extends KraPersistableBusinessObjectBase implements PersistableAttachment{ 
-    
+public class SubAwardForms extends KraPersistableBusinessObjectBase implements PersistableAttachment {
+
     private static final long serialVersionUID = 1L;
 
-    private String formId; 
-    private String description; 
+    private String formId;
+    private String description;
     private byte[] attachmentContent;
     private String fileName;
     private String contentType;
-    private transient FormFile templateFile;   
+    private transient FormFile templateFile;
     private Integer templateTypeCode;
     private SubawardTemplateType subAwardTemplateType;
     private Boolean selectToPrint = false;
@@ -57,160 +56,192 @@ public class SubAwardForms extends KraPersistableBusinessObjectBase implements P
     public final void setSelectToPrint(Boolean selectToPrint) {
         this.selectToPrint = selectToPrint;
     }
-    
+
     /**
      * Constructs a SubAwardForms.java.
      */
     public SubAwardForms() {
         super();
     }
-   
-    
-    /**.
+
+    /**
+     * .
      * This is the Getter Method for SubAwardTemplateType
+     *
      * @return Returns the SubAwardTemplateType.
-       */
+     */
     public SubawardTemplateType getSubAwardTemplateType() {
         return subAwardTemplateType;
     }
 
-    /**.
+    /**
+     * .
      * This is the Setter Method for subAwardTemplateType
+     *
      * @param subAwardTemplateType The subAwardTemplateType to set.
      */
     public void setSubAwardTemplateType(SubawardTemplateType subAwardTemplateType) {
         this.subAwardTemplateType = subAwardTemplateType;
     }
-    
-    /**.
+
+    /**
+     * .
      * This is the Getter Method for templateTypeCode
+     *
      * @return Returns the templateTypeCode.
-       */
+     */
     public Integer getTemplateTypeCode() {
         return templateTypeCode;
     }
 
-    /**.
+    /**
+     * .
      * This is the Setter Method for templateTypeCode
+     *
      * @param templateTypeCode The templateTypeCode to set.
      */
     public void setTemplateTypeCode(Integer templateTypeCode) {
         this.templateTypeCode = templateTypeCode;
     }
 
+    /**
+     * .
+     * This is the Getter Method for formId
+     *
+     * @return Returns the formId.
+     */
+    public String getFormId() {
+        return formId;
+    }
 
-  /**.
-   * This is the Getter Method for formId
-   * @return Returns the formId.
-	 */
-	public String getFormId() {
-		return formId;
-	}
+    /**
+     * .
+     * This is the Setter Method for formId
+     *
+     * @param formId The formId to set.
+     */
+    public void setFormId(String formId) {
+        this.formId = formId;
+    }
 
+    /**
+     * .
+     * This is the Getter Method for description
+     *
+     * @return Returns the description.
+     */
+    public String getDescription() {
+        return description;
+    }
 
-	/**.
-	 * This is the Setter Method for formId
-	 * @param formId The formId to set.
-	 */
-	public void setFormId(String formId) {
-		this.formId = formId;
-	}
+    /**
+     * .
+     * This is the Setter Method for description
+     *
+     * @param description The description to set.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-
-	/**.
-	 * This is the Getter Method for description
-	 * @return Returns the description.
-	 */
-	public String getDescription() {
-		return description;
-	}
-
-
-	/**.
-	 * This is the Setter Method for description
-	 * @param description The description to set.
-	 */
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-
-	/**.
-	 * This is the Getter Method for attachmentContent
-	 * @return Returns the attachmentContent.
-	 */
+    /**
+     * .
+     * This is the Getter Method for attachmentContent
+     *
+     * @return Returns the attachmentContent.
+     */
     @Override
-	public byte[] getAttachmentContent() {
-		return attachmentContent;
-	}
+    public byte[] getAttachmentContent() {
+        return attachmentContent;
+    }
 
-
-	/**.
-	 * This is the Setter Method for attachmentContent
-	 * @param attachmentContent The attachmentContent to set.
-	 */
+    /**
+     * .
+     * This is the Setter Method for attachmentContent
+     *
+     * @param attachmentContent The attachmentContent to set.
+     */
     @Override
-	public void setAttachmentContent(byte[] attachmentContent) {
-		this.attachmentContent = attachmentContent;
-	}
+    public void setAttachmentContent(byte[] attachmentContent) {
+        this.attachmentContent = attachmentContent;
+    }
 
-
-	/**.
-	 * This is the Getter Method for fileName
-	 * @return Returns the fileName.
-	 */
+    /**
+     * .
+     * This is the Getter Method for fileName
+     *
+     * @return Returns the fileName.
+     */
     @Override
-	public String getFileName() {
-		return fileName;
-	}
+    public String getFileName() {
+        return fileName;
+    }
 
-
-	/**.
-	 * This is the Setter Method for fileName
-	 * @param fileName The fileName to set.
-	 */
+    /**
+     * .
+     * This is the Setter Method for fileName
+     *
+     * @param fileName The fileName to set.
+     */
     @Override
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 
-
-	/**.
-	 * This is the Getter Method for contentType
-	 * @return Returns the contentType.
-	 */
+    /**
+     * .
+     * This is the Getter Method for contentType
+     *
+     * @return Returns the contentType.
+     */
     @Override
-	public String getContentType() {
-		return contentType;
-	}
+    public String getContentType() {
+        return contentType;
+    }
 
-
-	/**.
-	 * This is the Setter Method for contentType
-	 * @param contentType The contentType to set.
-	 */
+    /**
+     * .
+     * This is the Setter Method for contentType
+     *
+     * @param contentType The contentType to set.
+     */
     @Override
-	public void setContentType(String contentType) {
-		this.contentType = contentType;
-	}
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
 
+    /**
+     * .
+     * This is the Getter Method for templateFile
+     *
+     * @return Returns the templateFile.
+     */
+    public FormFile getTemplateFile() {
+        return templateFile;
+    }
 
-	/**.
-	 * This is the Getter Method for templateFile
-	 * @return Returns the templateFile.
-	 */
-	public FormFile getTemplateFile() {
-		return templateFile;
-	}
+    /**
+     * .
+     * This is the Setter Method for templateFile
+     *
+     * @param templateFile The templateFile to set.
+     */
+    public void setTemplateFile(FormFile templateFile) {
+        this.templateFile = templateFile;
+    }
 
-
-	/**.
-	 * This is the Setter Method for templateFile
-	 * @param templateFile The templateFile to set.
-	 */
-	public void setTemplateFile(FormFile templateFile) {
-		this.templateFile = templateFile;
-	}
-   
-
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(1000);
+        sb.append("[formId] = ");
+        sb.append(formId);
+        sb.append(", [description] = ");
+        sb.append(description);
+        sb.append(", [fileName] = ");
+        sb.append(fileName);
+        sb.append(", [contentType] = ");
+        sb.append(contentType);
+        
+        return sb.toString();
+    }
 }
