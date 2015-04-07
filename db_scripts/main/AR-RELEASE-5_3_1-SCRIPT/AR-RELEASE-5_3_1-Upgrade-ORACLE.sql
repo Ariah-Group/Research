15168a8d1f224bb86fe24425445ce07268d687bc
Ariah Group <code@ariahgroup.org>
2015-04-06 21:42:04 -0400

    fix for RES-200 , Budget - BUDGET_DETAILS table BUDGET_CATEGORY_CODE field incorrectly created as NUMBER instead of VARCHAR

---
+++ db_scripts/main/AR-RELEASE-5_3_1-SCRIPT/AR-RELEASE-5_3_1-Upgrade-ORACLE.sql
@@ -0,0 +1,16 @@
+set define off
+set sqlblanklines on
+spool AR-RELEASE-5_3_1-Upgrade-ORACLE-Install.log
+
+
+-- convert BUDGET_DETAILS.BUDGET_CATEGORY_CODE from NUMBER to VARCHAR
+alter table BUDGET_DETAILS add BUDGET_CATEGORY_CODE varchar2(3);
+update BUDGET_DETAILS set BUDGET_CATEGORY_CODE_TEMP=TO_CHAR(BUDGET_CATEGORY_CODE);
+alter table BUDGET_DETAILS drop column BUDGET_CATEGORY_CODE;
+alter table BUDGET_DETAILS add BUDGET_CATEGORY_CODE varchar2(3);
+update BUDGET_DETAILS set BUDGET_CATEGORY_CODE = BUDGET_CATEGORY_CODE_TEMP;
+
+
+
+commit;
+exit
