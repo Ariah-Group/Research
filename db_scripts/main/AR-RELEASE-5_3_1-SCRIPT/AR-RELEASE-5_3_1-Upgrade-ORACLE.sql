51de8327820e13c6dc655a262697a677e453f86b
Ariah Group <code@ariahgroup.org>
2015-04-09 23:59:56 -0400

    Implement RES-285 , IRB - New Sequence for Protocol Special Reviews

--- db_scripts/main/AR-RELEASE-5_3_1-SCRIPT/AR-RELEASE-5_3_1-Upgrade-ORACLE.sql
+++ db_scripts/main/AR-RELEASE-5_3_1-SCRIPT/AR-RELEASE-5_3_1-Upgrade-ORACLE.sql
@@ -72,12 +72,20 @@ Declare
   execute immediate 'CREATE SEQUENCE "SEQ_IRB_PROTOCOL_REFERENCE_ID" MINVALUE 1 MAXVALUE 999999999999999999 INCREMENT BY 1 START WITH ' || nextnum || ' NOCACHE  ORDER  NOCYCLE';
 END;
 
--- Generate new sequence for IRB Protocol Funding Soucres, using max of existing protocol funding sources
+-- Generate new sequence for IRB Protocol Funding Sources, using max of existing protocol funding sources
 Declare
   nextnum NUMBER;
  BEGIN
   select (nvl(max(PROTOCOL_FUNDING_SOURCE_ID),0)+1) into nextnum from PROTOCOL_FUNDING_SOURCE;
   execute immediate 'CREATE SEQUENCE "SEQ_IRB_PROTOCOL_FUNDSRC_ID" MINVALUE 1 MAXVALUE 999999999999999999 INCREMENT BY 1 START WITH ' || nextnum || ' NOCACHE  ORDER  NOCYCLE';
+END;
+
+-- Generate new sequence for IRB Protocol Special Reviews, using max of existing protocol special reviews
+Declare
+  nextnum NUMBER;
+ BEGIN
+  select (nvl(max(PROTOCOL_SPECIAL_REVIEW_ID),0)+1) into nextnum from PROTOCOL_SPECIAL_REVIEW;
+  execute immediate 'CREATE SEQUENCE "SEQ_IRB_PROTOCOL_SPECREV_ID" MINVALUE 1 MAXVALUE 999999999999999999 INCREMENT BY 1 START WITH ' || nextnum || ' NOCACHE  ORDER  NOCYCLE';
 END;
 
 
