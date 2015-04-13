fd3ff46fe11976c49d66cf27414cb2f6ab80a375
Ariah Group <code@ariahgroup.org>
2015-04-13 15:15:45 -0400

    Implement RES-301 , IRB - New Sequence for Protocol Submission Doc

--- db_scripts/main/AR-RELEASE-5_3_1-SCRIPT/AR-RELEASE-5_3_1-Upgrade-ORACLE.sql
+++ db_scripts/main/AR-RELEASE-5_3_1-SCRIPT/AR-RELEASE-5_3_1-Upgrade-ORACLE.sql
@@ -226,6 +226,18 @@ END;
 
 
 
+-- Generate new sequence for IRB Protocol Submission Doc, using max of existing protocol Submission Doc
+Declare
+  nextnum NUMBER;
+ BEGIN
+  select (nvl(max(SUBMISSION_DOC_ID),0)+1) into nextnum from PROTOCOL_SUBMISSION_DOC;
+  execute immediate 'CREATE SEQUENCE "SEQ_IRB_PROTOCOL_SUBMDOC_ID" MINVALUE 1 MAXVALUE 999999999999999999 INCREMENT BY 1 START WITH ' || nextnum || ' NOCACHE  ORDER  NOCYCLE';
+END;
+
+
+
+
+
 -- module-dsiable feature for funding source type
 alter table FUNDING_SOURCE_TYPE add DISABLED_COEUS_MODULE_ID NUMBER;
 update FUNDING_SOURCE_TYPE set DISABLED_COEUS_MODULE_ID=0;
