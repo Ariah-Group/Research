bdf7d062b869911c6f8953854e9fe044f58bcca8
Ariah Group <code@ariahgroup.org>
2015-06-13 23:11:20 -0400

    Reduction in cross-module dependencies by replacing hard usages of specific classes creating compile-time dependencies with existing static constants.

--- ar_webapp/src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/ProposalNarrativeTypeValuesFinder.java
+++ ar_webapp/src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/ProposalNarrativeTypeValuesFinder.java
@@ -49,50 +49,53 @@ import org.kuali.rice.krad.service.KRADServiceLocator;
 import java.util.*;
 
 /**
- * Finds the available set of supported Narrative Types.  See
- * the method <code>getKeyValues()</code> for a full description.
- * 
+ * Finds the available set of supported Narrative Types. See the method
+ * <code>getKeyValues()</code> for a full description.
+ *
  * @author KRADEV team
- * 
+ *
  * Rewrite of class to implement filterable KeyValue list.
- * 
- * A complete rewrite had to be done because super class was not editable, but the patterns used 
- * here should be incorporated into super class. This class would then be refactored to again rely
- * more on super class behavior. Super class rewrite should include isolating behaviors relying on 
- * infrastructure into protected methods to allow unit tests to override with mocks and stub out 
- * database and service lookups. 
+ *
+ * A complete rewrite had to be done because super class was not editable, but
+ * the patterns used here should be incorporated into super class. This class
+ * would then be refactored to again rely more on super class behavior. Super
+ * class rewrite should include isolating behaviors relying on infrastructure
+ * into protected methods to allow unit tests to override with mocks and stub
+ * out database and service lookups.
  */
-public class ProposalNarrativeTypeValuesFinder  extends FormViewAwareUifKeyValuesFinderBase {
+public class ProposalNarrativeTypeValuesFinder extends FormViewAwareUifKeyValuesFinderBase {
+
     private static final String NO = "N";
     private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(ProposalNarrativeTypeValuesFinder.class);
     private ParameterService parameterService;
 
     /**
      * Looks up and returns the ParameterService.
-     * @return the parameter service. 
+     *
+     * @return the parameter service.
      */
     protected ParameterService getParameterService() {
         if (this.parameterService == null) {
-            this.parameterService = KraServiceLocator.getService(ParameterService.class);        
+            this.parameterService = KraServiceLocator.getService(ParameterService.class);
         }
         return this.parameterService;
     }
-    
+
     @Override
     public List<KeyValue> getKeyValues() {
         Collection<NarrativeType> allNarrativeTypes = loadAllNarrativeTypes();
         return getFilteredKeyValues(allNarrativeTypes);
     }
-    
+
     public List<KeyValue> getFilteredKeyValues(Collection<NarrativeType> allNarrativeTypes) {
         Collection<NarrativeType> filteredCollection = filterCollection(allNarrativeTypes);
         return buildKeyValuesCollection(filteredCollection);
-        
+
     }
 
     protected Collection<NarrativeType> filterCollection(Collection<NarrativeType> narrativeTypes) {
         ProposalDevelopmentDocument document = getDocumentFromForm();
-        Collection<NarrativeType> forRemoval = new ArrayList<NarrativeType>();        
+        Collection<NarrativeType> forRemoval = new ArrayList<NarrativeType>();
         for (NarrativeType narrativeType : narrativeTypes) {
             for (Narrative narrative : document.getDevelopmentProposal().getNarratives()) {
                 if (filterCondition(narrative.getNarrativeType(), narrativeType)) {
@@ -100,12 +103,12 @@ public class ProposalNarrativeTypeValuesFinder  extends FormViewAwareUifKeyValue
                 } else {
                     LOG.debug("Not removing narrative type " + narrativeType.getDescription());
                 }
-            }            
+            }
         }
-        
+
         // Remove any instances for with these type codes already in the document
         narrativeTypes.removeAll(forRemoval);
-        
+
         return narrativeTypes;
     }
 
@@ -119,40 +122,42 @@ public class ProposalNarrativeTypeValuesFinder  extends FormViewAwareUifKeyValue
         }
         return KeyValues;
     }
-    
+
     @SuppressWarnings("unchecked")
     protected Collection<NarrativeType> loadAllNarrativeTypes() {
         ProposalDevelopmentDocument document = getDocumentFromForm();
         DevelopmentProposal developmentProposal = document.getDevelopmentProposal();
         List<NarrativeType> validS2SFormNarratives = new ArrayList<NarrativeType>();
-        if(isS2sCandidate(developmentProposal)){
-            populateValidFormNarratives(developmentProposal,validS2SFormNarratives);
+        if (isS2sCandidate(developmentProposal)) {
+            populateValidFormNarratives(developmentProposal, validS2SFormNarratives);
             populateGenericValidNarrativeTypes(validS2SFormNarratives);
-            populateValidNarrativeTypeFromParentProposal(developmentProposal,validS2SFormNarratives);
-        }else{
-            String proposalNarrativeTypeGroup = this.getParameterService().getParameterValueAsString(ProposalDevelopmentDocument.class, Constants.PROPOSAL_NARRATIVE_TYPE_GROUP);
-            Map<String,String> queryMap = new HashMap<String,String>();
+            populateValidNarrativeTypeFromParentProposal(developmentProposal, validS2SFormNarratives);
+        } else {
+            String proposalNarrativeTypeGroup = this.getParameterService().getParameterValueAsString(
+                    Constants.MODULE_NAMESPACE_PROPOSAL_DEVELOPMENT,
+                    Constants.PARAMETER_COMPONENT_DOCUMENT,
+                    Constants.PROPOSAL_NARRATIVE_TYPE_GROUP);
+            Map<String, String> queryMap = new HashMap<String, String>();
             queryMap.put("narrativeTypeGroup", proposalNarrativeTypeGroup);
-            List<ValidNarrForms> validNarrativeForms = (List<ValidNarrForms>)getBusinessObjectService().findAll(ValidNarrForms.class);
-            List<NarrativeType> allNarrativeTypes = (List)getBusinessObjectService().findMatching(NarrativeType.class, queryMap);
-            validS2SFormNarratives = removeValidNarrativeForms(allNarrativeTypes,validNarrativeForms);
+            List<ValidNarrForms> validNarrativeForms = (List<ValidNarrForms>) getBusinessObjectService().findAll(ValidNarrForms.class);
+            List<NarrativeType> allNarrativeTypes = (List) getBusinessObjectService().findMatching(NarrativeType.class, queryMap);
+            validS2SFormNarratives = removeValidNarrativeForms(allNarrativeTypes, validNarrativeForms);
         }
         return validS2SFormNarratives;
     }
 
-
-    private List<NarrativeType> removeValidNarrativeForms(List<NarrativeType> narrativeTypes,List<ValidNarrForms> validNarrativeForms) {
+    private List<NarrativeType> removeValidNarrativeForms(List<NarrativeType> narrativeTypes, List<ValidNarrForms> validNarrativeForms) {
         List<NarrativeType> filteredNarrativeTypes = new ArrayList<NarrativeType>();
         populateGenericValidNarrativeTypes(filteredNarrativeTypes);
         for (NarrativeType narrativeType : narrativeTypes) {
             boolean exists = false;
             for (ValidNarrForms validNarrForm : validNarrativeForms) {
-                if(validNarrForm.getNarrativeTypeCode().equals(narrativeType.getNarrativeTypeCode())){
+                if (validNarrForm.getNarrativeTypeCode().equals(narrativeType.getNarrativeTypeCode())) {
                     exists = true;
                     break;
                 }
             }
-            if(!exists){
+            if (!exists) {
                 filteredNarrativeTypes.add(narrativeType);
             }
         }
@@ -165,25 +170,28 @@ public class ProposalNarrativeTypeValuesFinder  extends FormViewAwareUifKeyValue
 
     /**
      * This method...
+     *
      * @return
      */
     private BusinessObjectService getBusinessObjectService() {
         return KRADServiceLocator.getBusinessObjectService();
     }
+
     public DevelopmentProposal getDevelopmentProposal(String proposalNumber) {
         Map<String, String> pk = new HashMap<String, String>();
         pk.put("proposalNumber", proposalNumber);
         return (DevelopmentProposal) (getBusinessObjectService().findByPrimaryKey(DevelopmentProposal.class, pk));
     }
+
     private List<NarrativeType> populateValidFormNarratives(DevelopmentProposal developmentProposal, List<NarrativeType> validaNarrativeTypes) {
         List<S2sOppForms> s2sOpportunityForms = developmentProposal.getS2sOppForms();
         for (S2sOppForms oppForms : s2sOpportunityForms) {
             Map<String, String> queryMap = new HashMap<String, String>();
             queryMap.put("formName", oppForms.getFormName());
-            List<ValidNarrForms>  validNarrativeForms = (List)getBusinessObjectService().findMatching(ValidNarrForms.class, queryMap);
+            List<ValidNarrForms> validNarrativeForms = (List) getBusinessObjectService().findMatching(ValidNarrForms.class, queryMap);
             for (ValidNarrForms validNarrForms : validNarrativeForms) {
-                if(isProposalGroup(validNarrForms)){
-                    if(!isNarrativeAlreadyAdded(validaNarrativeTypes,validNarrForms.getNarrativeType()) && validNarrForms.getNarrativeType().getSystemGenerated().equals("N")){
+                if (isProposalGroup(validNarrForms)) {
+                    if (!isNarrativeAlreadyAdded(validaNarrativeTypes, validNarrForms.getNarrativeType()) && validNarrForms.getNarrativeType().getSystemGenerated().equals("N")) {
                         validaNarrativeTypes.add(validNarrForms.getNarrativeType());
                     }
                 }
@@ -191,36 +199,38 @@ public class ProposalNarrativeTypeValuesFinder  extends FormViewAwareUifKeyValue
         }
         return validaNarrativeTypes;
     }
+
     private boolean isNarrativeAlreadyAdded(List<NarrativeType> validaNarrativeTypes, NarrativeType validNarrativeType) {
         for (NarrativeType narrativeType : validaNarrativeTypes) {
-            if(narrativeType.getNarrativeTypeCode().equals(validNarrativeType.getNarrativeTypeCode())){
+            if (narrativeType.getNarrativeTypeCode().equals(validNarrativeType.getNarrativeTypeCode())) {
                 return true;
             }
         }
         return false;
     }
-    String proposalNarrativeTypeGroup = this.getParameterService().getParameterValueAsString(ProposalDevelopmentDocument.class, Constants.PROPOSAL_NARRATIVE_TYPE_GROUP);
+    String proposalNarrativeTypeGroup = this.getParameterService().getParameterValueAsString(
+            Constants.MODULE_NAMESPACE_PROPOSAL_DEVELOPMENT,
+                Constants.PARAMETER_COMPONENT_DOCUMENT,
+            Constants.PROPOSAL_NARRATIVE_TYPE_GROUP);
 
     private boolean isProposalGroup(ValidNarrForms validNarrForms) {
         return proposalNarrativeTypeGroup.equals(validNarrForms.getNarrativeType().getNarrativeTypeGroup());
     }
 
-
     private void populateValidNarrativeTypeFromParentProposal(DevelopmentProposal developmentProposal, List<NarrativeType> validaNarrativeTypes) {
-        if(developmentProposal.isInHierarchy()){
+        if (developmentProposal.isInHierarchy()) {
             String hierarchyProposalNumber = developmentProposal.getHierarchyParentProposalNumber();
             DevelopmentProposal parentProposal = getDevelopmentProposal(hierarchyProposalNumber);
             populateValidFormNarratives(parentProposal, validaNarrativeTypes);
         }
     }
 
-
     private Map<String, String> populateGenericValidNarrativeTypes(List<NarrativeType> validaNarrativeTypes) {
-        Map<String,String> queryMap = new HashMap<String,String>();
+        Map<String, String> queryMap = new HashMap<String, String>();
         queryMap.put("formName", "ALL");
-        List<ValidNarrForms> validNarrativeForms = (List)getBusinessObjectService().findMatching(ValidNarrForms.class, queryMap);
+        List<ValidNarrForms> validNarrativeForms = (List) getBusinessObjectService().findMatching(ValidNarrForms.class, queryMap);
         for (ValidNarrForms validNarrForms : validNarrativeForms) {
-            if(isProposalGroup(validNarrForms)){
+            if (isProposalGroup(validNarrForms)) {
                 validaNarrativeTypes.add(validNarrForms.getNarrativeType());
             }
         }
@@ -230,7 +240,6 @@ public class ProposalNarrativeTypeValuesFinder  extends FormViewAwareUifKeyValue
     protected ProposalDevelopmentDocument getDocumentFromForm() {
         return (ProposalDevelopmentDocument) getDocument();
     }
-    
 
     protected boolean filterCondition(NarrativeType documentNarrativeType, NarrativeType narrativeType) {
         return documentNarrativeType.getNarrativeTypeCode().equals(narrativeType.getNarrativeTypeCode()) && multiplesNotAllowed(narrativeType);
