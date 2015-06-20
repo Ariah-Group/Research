c12735ce3f3b50937d0afabe5f932894c2de8d0d
Ariah Group <code@ariahgroup.org>
2015-06-19 21:41:03 -0400

    removing unneeded dependencies

--- ar_webapp/src/main/java/org/kuali/kra/iacuc/threers/AddAlternateSearchRule.java
+++ ar_webapp/src/main/java/org/kuali/kra/iacuc/threers/AddAlternateSearchRule.java
@@ -15,7 +15,6 @@
  */
 package org.kuali.kra.iacuc.threers;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.kuali.kra.infrastructure.KeyConstants;
 import org.kuali.kra.rule.BusinessRuleInterface;
@@ -30,7 +29,7 @@ public class AddAlternateSearchRule extends ResearchDocumentRuleBase implements 
 
     private static final String ERROR_PATH_PREFIX = "iacucAlternateSearchHelper.newAlternateSearch.";
     private static final String DD_ENTRY_NAME = "IacucAlternateSearch";
-        
+
     @Override
     public boolean processRules(AddAlternateSearchEvent event) {
         return validateRequiredFields(event);
@@ -38,89 +37,89 @@ public class AddAlternateSearchRule extends ResearchDocumentRuleBase implements 
 
     private boolean validateRequiredFields(AddAlternateSearchEvent event) {
         boolean valid = true;
-        
+
         valid &= validateSearchDate(event);
         valid &= validateDatabases(event);
         valid &= validateYears(event);
         valid &= validateKeywords(event);
-        
+
         return valid;
     }
-    
+
     private boolean validateSearchDate(AddAlternateSearchEvent event) {
         Date searchDate = event.getAlternateSearch().getSearchDate();
-        
+
         if (searchDate != null) {
-            DictionaryValidationResult result = getDictionaryValidationService().validate(searchDate, DD_ENTRY_NAME, "searchDate", false); 
-            if (result.getNumberOfErrors() == 0){
+            DictionaryValidationResult result = getDictionaryValidationService().validate(searchDate, DD_ENTRY_NAME, "searchDate", false);
+            if (result.getNumberOfErrors() == 0) {
                 Date currentDate = new Date();
                 if (!searchDate.after(currentDate)) {
                     return true;
                 } else {
-                    GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "searchDate", 
-                            KeyConstants.ERROR_IACUC_VALIDATION_SEARCHDATE_AFTER_CURRENTDATE, new String[] { "Search Date" });                
-                    return false;                    
+                    GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "searchDate",
+                            KeyConstants.ERROR_IACUC_VALIDATION_SEARCHDATE_AFTER_CURRENTDATE, new String[]{"Search Date"});
+                    return false;
                 }
             } else {
-                GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "searchDate", 
-                        KeyConstants.ERROR_IACUC_VALIDATION_ALTERNATE_SEARCH, new String[] { "Search Date" });                
-                return false;
-            }            
-        } else {
-            GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "searchDate", 
-                    KeyConstants.ERROR_IACUC_VALIDATION_ALTERNATE_SEARCH, new String[] { "Search Date" });                
-            return false;
-        }
-    }
-    
-    private boolean validateDatabases(AddAlternateSearchEvent event) {
-        List<String> newDatabases = event.getSelectedDatabases();
-        
-        if (CollectionUtils.isNotEmpty(newDatabases)) {
-            return true;
-        } else {
-            GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "databases", 
-                    KeyConstants.ERROR_REQUIRED, new String[] { "Databases" }); 
-            return false;
-        }
-    }
-    
-    private boolean validateYears(AddAlternateSearchEvent event) {
-        String years = event.getAlternateSearch().getYearsSearched();
-        
-        if (StringUtils.isNotBlank(years)) {
-            DictionaryValidationResult result = getDictionaryValidationService().validate((Object)years, DD_ENTRY_NAME, "yearsSearched", false); 
-            if (result.getNumberOfErrors() == 0){
-                return true;
-            } else {
-                GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "yearsSearched", 
-                        KeyConstants.ERROR_IACUC_VALIDATION_ALTERNATE_SEARCH, new String[] { "Years" });                
+                GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "searchDate",
+                        KeyConstants.ERROR_IACUC_VALIDATION_ALTERNATE_SEARCH, new String[]{"Search Date"});
                 return false;
             }
         } else {
-            GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX, 
-                    KeyConstants.ERROR_REQUIRED, new String[] { "Years" }); 
-            return false;            
+            GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "searchDate",
+                    KeyConstants.ERROR_IACUC_VALIDATION_ALTERNATE_SEARCH, new String[]{"Search Date"});
+            return false;
+        }
+    }
+
+    private boolean validateDatabases(AddAlternateSearchEvent event) {
+        List<String> newDatabases = event.getSelectedDatabases();
+
+        if (newDatabases != null && !newDatabases.isEmpty()) {
+            return true;
+        } else {
+            GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "databases",
+                    KeyConstants.ERROR_REQUIRED, new String[]{"Databases"});
+            return false;
+        }
+    }
+
+    private boolean validateYears(AddAlternateSearchEvent event) {
+        String years = event.getAlternateSearch().getYearsSearched();
+
+        if (StringUtils.isNotBlank(years)) {
+            DictionaryValidationResult result = getDictionaryValidationService().validate((Object) years, DD_ENTRY_NAME, "yearsSearched", false);
+            if (result.getNumberOfErrors() == 0) {
+                return true;
+            } else {
+                GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "yearsSearched",
+                        KeyConstants.ERROR_IACUC_VALIDATION_ALTERNATE_SEARCH, new String[]{"Years"});
+                return false;
+            }
+        } else {
+            GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX,
+                    KeyConstants.ERROR_REQUIRED, new String[]{"Years"});
+            return false;
         }
     }
 
     private boolean validateKeywords(AddAlternateSearchEvent event) {
         String keywords = event.getAlternateSearch().getKeywords();
-        
+
         if (StringUtils.isNotBlank(keywords)) {
-            DictionaryValidationResult result = getDictionaryValidationService().validate((Object)keywords, DD_ENTRY_NAME, "keywords", false); 
-            if (result.getNumberOfErrors() == 0){
+            DictionaryValidationResult result = getDictionaryValidationService().validate((Object) keywords, DD_ENTRY_NAME, "keywords", false);
+            if (result.getNumberOfErrors() == 0) {
                 return true;
             } else {
-                GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "keywords", 
-                        KeyConstants.ERROR_IACUC_VALIDATION_ALTERNATE_SEARCH, new String[] { "Keywords" });                
+                GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX + "keywords",
+                        KeyConstants.ERROR_IACUC_VALIDATION_ALTERNATE_SEARCH, new String[]{"Keywords"});
                 return false;
             }
         } else {
-            GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX, 
-                    KeyConstants.ERROR_REQUIRED, new String[] { "Keywords" }); 
-            return false;            
+            GlobalVariables.getMessageMap().putError(ERROR_PATH_PREFIX,
+                    KeyConstants.ERROR_REQUIRED, new String[]{"Keywords"});
+            return false;
         }
-    }    
+    }
 
 }
