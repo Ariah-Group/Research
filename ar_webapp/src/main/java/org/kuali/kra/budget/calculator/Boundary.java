180f9dfb4fd3f3d9c582385ba8fc371b46dcd52c
Ariah Group <code@ariahgroup.org>
2015-06-26 13:46:34 -0400

    minor tweaks / improvements

--- ar_webapp/src/main/java/org/kuali/kra/budget/calculator/Boundary.java
+++ ar_webapp/src/main/java/org/kuali/kra/budget/calculator/Boundary.java
@@ -23,166 +23,182 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
- * This class holds the date boundaries ie, Start Date & End Date. Also
- * have methods which to get the date related values like the no of days in
+ * This class holds the date boundaries ie, Start Date & End Date. Also have
+ * methods which to get the date related values like the no of days in
  * StartDate, month of StartDate, etc.
  *
  */
 public class Boundary {
-    private Date startDate; 
-    private Date endDate; 
+
+    private Date startDate;
+    private Date endDate;
     private Calendar calendar = Calendar.getInstance();
     private DateTimeService dateTimeService;
     private BudgetDecimal applicableCost;
     private BudgetDecimal applicableCostSharing;
-    
-/**
- * Default Constructor...
- */
-    public  Boundary() {
+
+    /**
+     * Default Constructor...
+     */
+    public Boundary() {
         init();
     }
-    
+
     protected void init() {
         dateTimeService = KraServiceLocator.getService(DateTimeService.class);
     }
 
-/**
- * Constructor...
- * Accepts 2 parameters, viz startDate & endDate and sets the values to the 
- * class variables
- * 
- * @param startDate 
- * @param endDate 
- */
-    public  Boundary(Date startDate, Date endDate) {   
+    /**
+     * Constructor... Accepts 2 parameters, viz startDate & endDate and sets the
+     * values to the class variables
+     *
+     * @param startDate
+     * @param endDate
+     */
+    public Boundary(Date startDate, Date endDate) {
         this();
         this.startDate = startDate;
         this.endDate = endDate;
-    } 
+    }
 
-    public int getNumberOfDays(){
+    public int getNumberOfDays() {
         return dateTimeService.dateDiff(getStartDate(), getEndDate(), true);
     }
-    
-/**
- * Returns the startDate Month
- * @return int
- */
-    public int getStartDateMonth() {        
+
+    /**
+     * Returns the startDate Month
+     *
+     * @return int
+     */
+    public int getStartDateMonth() {
         calendar.setTime(startDate);
-        return calendar.get(Calendar.MONTH)+1;
-    }        
-/**
- * Returns the total no. of days in startDate
- * 
- * @return int
- */
-    public int getStartDateTotalDays() {        
+        return calendar.get(Calendar.MONTH) + 1;
+    }
+
+    /**
+     * Returns the total no. of days in startDate
+     *
+     * @return int
+     */
+    public int getStartDateTotalDays() {
         calendar.setTime(startDate);
         return calendar.getActualMaximum(Calendar.DATE);
     } // end getStartDateNoOfDays        
 
-/**
- * Returns the remaining no. of days in startDate
- * 
- * @return int
- */
-    public int getStartDateRemainingDays() {        
+    /**
+     * Returns the remaining no. of days in startDate
+     *
+     * @return int
+     */
+    public int getStartDateRemainingDays() {
         calendar.setTime(startDate);
         return calendar.getActualMaximum(Calendar.DATE) - calendar.get(Calendar.DATE) + 1;
     } // end getStartDateNoOfDays       
 
-/**
- * Returns the endDate Month * 
- * 
- * @return int
- */
-    public int getEndDateMonth() {        
+    /**
+     * Returns the endDate Month *
+     *
+     * @return int
+     */
+    public int getEndDateMonth() {
         calendar.setTime(endDate);
-        return calendar.get(Calendar.MONTH)+1;
+        return calendar.get(Calendar.MONTH) + 1;
     }
 
-/**
- * Returns the total no. of days in endDate
- * 
- * @return int
- */
-    public int getEndDateTotalDays() {        
+    /**
+     * Returns the total no. of days in endDate
+     *
+     * @return int
+     */
+    public int getEndDateTotalDays() {
         calendar.setTime(endDate);
         return calendar.getActualMaximum(Calendar.DATE);
-    }        
+    }
 
-/**
- * Returns the no. of days in endDate
- * 
- * @return int
- */
-    public int getEndDateNoOfDays() {        
+    /**
+     * Returns the no. of days in endDate
+     *
+     * @return int
+     */
+    public int getEndDateNoOfDays() {
         calendar.setTime(endDate);
         return calendar.get(Calendar.DATE);
-    }   
-
-/**
- * Returns the no. of days between the startDate & endDate
- * 
- * @return long
- */
-    public long getDateDifference() {
-        return Math.round((endDate.getTime() - startDate.getTime())/86400000.0d + 1);
     }
-    
-    /** Getter for property startDate.
+
+    /**
+     * Returns the no. of days between the startDate & endDate
+     *
+     * @return long
+     */
+    public long getDateDifference() {
+        return Math.round((endDate.getTime() - startDate.getTime()) / 86400000.0d + 1);
+    }
+
+    /**
+     * Getter for property startDate.
+     *
      * @return Value of property startDate.
      */
     public java.util.Date getStartDate() {
         return startDate;
     }
-    
-    /** Setter for property startDate.
+
+    /**
+     * Setter for property startDate.
+     *
      * @param startDate New value of property startDate.
      */
     public void setStartDate(java.util.Date startDate) {
         this.startDate = startDate;
     }
-    
-    /** Getter for property endDate.
+
+    /**
+     * Getter for property endDate.
+     *
      * @return Value of property endDate.
      */
     public java.util.Date getEndDate() {
         return endDate;
     }
-    
-    /** Setter for property endDate.
+
+    /**
+     * Setter for property endDate.
+     *
      * @param endDate New value of property endDate.
      */
     public void setEndDate(java.util.Date endDate) {
         this.endDate = endDate;
     }
+
     /**
      * Overridden method of toString. It will form a string representation of
      * each element associated with this class.
-     * @return Concatinated string representation of each element 
+     *
+     * @return Concatenated string representation of each element
      */
-    public String toString(){
-        StringBuffer strBffr = new StringBuffer("");
-        strBffr.append("Start date=>"+startDate);
-        strBffr.append(";");
-        strBffr.append("End date=>"+endDate);  
+    @Override
+    public String toString() {
+        StringBuilder strBffr = new StringBuilder(50);
+        strBffr.append("Start date=>");
+        strBffr.append(startDate);
+        strBffr.append(";End date=>");
+        strBffr.append(endDate);
         strBffr.append("\n");
         return strBffr.toString();
     }
 
     /**
-     * Gets the applicableCost attribute. 
+     * Gets the applicableCost attribute.
+     *
      * @return Returns the applicableCost.
      */
     public BudgetDecimal getApplicableCost() {
-        return applicableCost==null?BudgetDecimal.ZERO:applicableCost;
+        return applicableCost == null ? BudgetDecimal.ZERO : applicableCost;
     }
 
     /**
      * Sets the applicableCost attribute value.
+     *
      * @param applicableCost The applicableCost to set.
      */
     public void setApplicableCost(BudgetDecimal applicableCost) {
@@ -190,7 +206,8 @@ public class Boundary {
     }
 
     /**
-     * Gets the applicableCostSharing attribute. 
+     * Gets the applicableCostSharing attribute.
+     *
      * @return Returns the applicableCostSharing.
      */
     public BudgetDecimal getApplicableCostSharing() {
@@ -199,13 +216,12 @@ public class Boundary {
 
     /**
      * Sets the applicableCostSharing attribute value.
+     *
      * @param applicableCostSharing The applicableCostSharing to set.
      */
     public void setApplicableCostSharing(BudgetDecimal applicableCostSharing) {
         this.applicableCostSharing = applicableCostSharing;
     }
-    
- } // end Boundary
 
-
+} // end Boundary
 
