085f9a17c5e0af398364802cc5275c2445d70f3d
Ariah Group <code@ariahgroup.org>
2015-06-26 11:07:11 -0400

    Fix for RES-428 , IRB - Fix Status Check Bug in When Protocol Is Administratively Closed

--- ar_webapp/src/main/java/org/kuali/kra/irb/actions/IrbProtocolActionRequestServiceImpl.java
+++ ar_webapp/src/main/java/org/kuali/kra/irb/actions/IrbProtocolActionRequestServiceImpl.java
@@ -12,22 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * ------------------------------------------------------
- * Updates made after January 1, 2015 are :
- * Copyright 2015 The Ariah Group, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package org.kuali.kra.irb.actions;
 
@@ -971,7 +955,7 @@ public class IrbProtocolActionRequestServiceImpl extends ProtocolActionRequestSe
         saveReviewComments(protocolForm, actionBean.getReviewCommentsBean());
         recordProtocolActionSuccess(ACTION_NAME_CLOSE);
         ProtocolNotificationRequestBean notificationBean = null;
-        if (ProtocolStatus.CLOSED_ADMINISTRATIVELY.equals(protocol.getProtocolStatus())) {
+        if (ProtocolStatus.CLOSED_ADMINISTRATIVELY.equals(protocol.getProtocolStatusCode())) {
             notificationBean = new ProtocolNotificationRequestBean(protocol, ProtocolActionType.CLOSED_ADMINISTRATIVELY_CLOSED, "Closed By Administrator");
         } else {
             notificationBean = new ProtocolNotificationRequestBean(protocol, ProtocolActionType.CLOSED_ADMINISTRATIVELY_CLOSED, "Closed By Investigator");
