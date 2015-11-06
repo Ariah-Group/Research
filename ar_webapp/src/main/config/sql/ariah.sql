3ce509c0a36f3efb0c9ef11cf206a25082477bdc
Ariah Group <code@ariahgroup.org>
2015-11-06 13:31:16 -0500

    Fix for RES-471, Negotiation - Admin User Cannot Create Negotiations By Default

--- ar_webapp/src/main/config/sql/ariah.sql
+++ ar_webapp/src/main/config/sql/ariah.sql
@@ -590,3 +590,7 @@ VALUES  ('KC-PD','Document','ARIAH_PROPDEV_ACTIONCODE_NARRATIVE_MODIFY', sys_gui
 -------------------
 -- Version 5.3.5 ---
 -------------------
+
+-- ensure ADMIN user can create and manage negotiation records
+Insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID,VER_NBR,OBJ_ID,ROLE_ID,MBR_ID,MBR_TYP_CD,ACTV_FRM_DT,ACTV_TO_DT,LAST_UPDT_DT) 
+values (KRIM_ROLE_MBR_ID_S.nextval,1,SYS_GUID(), (select ROLE_ID FROM KRIM_ROLE_T where ROLE_NM='Negotiation Administrator') ,'admin','P',null,null,SYSDATE);
--- db_scripts/main/AR-RELEASE-5_3_5-SCRIPT/KR-RELEASE-5_3_5-Upgrade-ORACLE.sql
+++ db_scripts/main/AR-RELEASE-5_3_5-SCRIPT/KR-RELEASE-5_3_5-Upgrade-ORACLE.sql
@@ -3,5 +3,10 @@ set sqlblanklines on
 spool KR-RELEASE-5_3_5-Upgrade-ORACLE-Install.log
 
 
+
+-- ensure ADMIN user can create and manage negotiation records
+Insert into KRIM_ROLE_MBR_T (ROLE_MBR_ID,VER_NBR,OBJ_ID,ROLE_ID,MBR_ID,MBR_TYP_CD,ACTV_FRM_DT,ACTV_TO_DT,LAST_UPDT_DT) 
+values (KRIM_ROLE_MBR_ID_S.nextval,1,SYS_GUID(), (select ROLE_ID FROM KRIM_ROLE_T where ROLE_NM='Negotiation Administrator') ,'admin','P',null,null,SYSDATE);
+
 commit;
 exit
