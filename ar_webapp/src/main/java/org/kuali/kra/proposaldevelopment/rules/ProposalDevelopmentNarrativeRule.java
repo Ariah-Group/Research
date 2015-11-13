a65e26d8acc60c47fe7c1344fb6ac6ea4b785627
Ariah Group <code@ariahgroup.org>
2015-11-13 16:05:41 -0500

    Fixed copy-paste error in header of some files.

--- ar_webapp/src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentNarrativeRule.java
+++ ar_webapp/src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentNarrativeRule.java
@@ -12,22 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * ------------------------------------------------------
- * Updates made after January 1, 2015 are :
- * Copyright 2015 The Ariah Group, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package org.kuali.kra.proposaldevelopment.rules;
 
@@ -386,7 +370,7 @@ public class ProposalDevelopmentNarrativeRule extends ResearchDocumentRuleBase i
      * Does this person have the necessary permission for the narrative right
      * they have been assigned? In other words, a person cannot be given the
      * narrative right of modify if they don't have the MODIFY_NARRATIVE
-     * permssion.
+     * permission.
      *
      * @param userRights the person's narrative right
      * @param doc the Proposal Development Document
