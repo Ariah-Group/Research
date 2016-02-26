11bb6200b7c679bdc953f81337066f025b96619b
Ariah Group <code@ariahgroup.org>
2016-02-25 22:52:04 -0500

    Implement RES-549

--- db_scripts/main/AR-RELEASE-5_4_0-SCRIPT/AR-RELEASE-5_4_0-Upgrade-ORACLE.sql
+++ db_scripts/main/AR-RELEASE-5_4_0-SCRIPT/AR-RELEASE-5_4_0-Upgrade-ORACLE.sql
@@ -10,6 +10,24 @@ Declare
   execute immediate 'CREATE SEQUENCE "SEQ_TBN_ID" MINVALUE 1 MAXVALUE 999999999999999999 INCREMENT BY 1 START WITH ' || nextnum || ' NOCACHE  ORDER  NOCYCLE';
 END;
 
+-- change TBN.TBN_ID from VARCHAR to NUMBER
+alter table TBN add TBN_ID_2 NUMBER;
+update TBN set TBN_ID_2 = TO_NUMBER(TBN_ID);
+ALTER TABLE    TBN drop constraint    TBNP1;
+update TBN set TBN_ID = null;
+alter table TBN modify (TBN_ID NUMBER);
+update TBN set TBN_ID = TBN_ID_2;
+alter table TBN ADD CONSTRAINT PK_TBNID PRIMARY KEY (TBN_ID);
+alter table TBN drop column TBN_ID_2;
+
+-- change BUDGET_PERSONS.TBN_ID from VARCHAR to NUMBER
+alter table BUDGET_PERSONS add TBN_ID_2 NUMBER;
+update BUDGET_PERSONS set TBN_ID_2 = TO_NUMBER(TBN_ID);
+update BUDGET_PERSONS set TBN_ID = null;
+alter table BUDGET_PERSONS modify (TBN_ID NUMBER);
+update BUDGET_PERSONS set TBN_ID = TBN_ID_2;
+alter table BUDGET_PERSONS drop column TBN_ID_2;
+
 
 commit;
 exit
