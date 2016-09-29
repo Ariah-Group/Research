77201b755c21d1ef1961cc13e40b54d91f0d5aeb
Ariah Group <code@ariahgroup.org>
2016-09-29 13:48:13 -0400

    minor tweak

--- ar_webapp/src/main/java/org/kuali/kra/lookup/keyvalue/LookupableBoValuesFinder.java
+++ ar_webapp/src/main/java/org/kuali/kra/lookup/keyvalue/LookupableBoValuesFinder.java
@@ -12,22 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * ------------------------------------------------------
- * Updates made after January 1, 2015 are :
- * Copyright 2015 The Ariah Group, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package org.kuali.kra.lookup.keyvalue;
 
@@ -50,55 +34,60 @@ public class LookupableBoValuesFinder extends UifKeyValuesFinderBase {
         DataDictionaryService dataDictionaryService = KraServiceLocator.getService(DataDictionaryService.class);
         // this only has entries that have been loaded - force load?
 
-    	Map<String, BusinessObjectEntry> businessObjectEntries = dataDictionaryService.getDataDictionary().getBusinessObjectEntries();
-    	
-    	Map<String, Integer> labelCounts = getLabelCounts(businessObjectEntries);
-    	
-    	for (String businessObject: businessObjectEntries.keySet()) {
-    	    org.kuali.rice.kns.datadictionary.BusinessObjectEntry businessObjectEntry = (org.kuali.rice.kns.datadictionary.BusinessObjectEntry) businessObjectEntries.get(businessObject);
-    	    if ((businessObjectEntry.hasLookupDefinition()) 
-    	            && (businessObject.startsWith("org.kuali.kra") || businessObject.startsWith("org.ariahgroup.research") 
-                            || businessObject.equals("org.kuali.rice.location.impl.campus.CampusBo")
-                            || businessObject.equals("org.kuali.rice.location.impl.country.CountryBo")
-                            || businessObject.equals("org.kuali.rice.location.impl.county.CountyBo")
-                            || businessObject.equals("org.kuali.rice.location.impl.postalcode.PostalCodeBo")
-                            || businessObject.equals("org.kuali.rice.location.impl.state.StateBo"))) {
-    	        String key = businessObject;
-    	        
-    	        String label = StringUtils.removeEnd(businessObjectEntry.getLookupDefinition().getTitle().trim()," Lookup");
-    	        
-    	        if (labelCounts.get(label) > 1){
-    	            label = label + " (" + key.substring(key.lastIndexOf('.') + 1) + ")";
-    	        }
+        try {
 
-        		KeyValue KeyValue = new ConcreteKeyValue(key, label);
-        		if (!keyValues.contains(KeyValue)) {
-        		    keyValues.add(KeyValue);
-        		}
-    	    }
-    	}
+            Map<String, BusinessObjectEntry> businessObjectEntries = dataDictionaryService.getDataDictionary().getBusinessObjectEntries();
 
-    	// added comparator below to alphabetize lists on label
-        Collections.sort(keyValues, new KeyValueComparator());
-        keyValues.add(0, new ConcreteKeyValue("", "select"));
+            Map<String, Integer> labelCounts = getLabelCounts(businessObjectEntries);
 
-    	return keyValues;
+            for (String businessObject : businessObjectEntries.keySet()) {
+                org.kuali.rice.kns.datadictionary.BusinessObjectEntry businessObjectEntry = (org.kuali.rice.kns.datadictionary.BusinessObjectEntry) businessObjectEntries.get(businessObject);
+                if ((businessObjectEntry.hasLookupDefinition())
+                        && (businessObject.startsWith("org.kuali.kra") || businessObject.startsWith("org.ariahgroup.research")
+                        || businessObject.equals("org.kuali.rice.location.impl.campus.CampusBo")
+                        || businessObject.equals("org.kuali.rice.location.impl.country.CountryBo")
+                        || businessObject.equals("org.kuali.rice.location.impl.county.CountyBo")
+                        || businessObject.equals("org.kuali.rice.location.impl.postalcode.PostalCodeBo")
+                        || businessObject.equals("org.kuali.rice.location.impl.state.StateBo"))) {
+                    String key = businessObject;
+
+                    String label = StringUtils.removeEnd(businessObjectEntry.getLookupDefinition().getTitle().trim(), " Lookup");
+
+                    if (labelCounts.get(label) > 1) {
+                        label = label + " (" + key.substring(key.lastIndexOf('.') + 1) + ")";
+                    }
+
+                    KeyValue KeyValue = new ConcreteKeyValue(key, label);
+                    if (!keyValues.contains(KeyValue)) {
+                        keyValues.add(KeyValue);
+                    }
+                }
+            }
+
+            // added comparator below to alphabetize lists on label
+            Collections.sort(keyValues, new KeyValueComparator());
+            keyValues.add(0, new ConcreteKeyValue("", "select"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return keyValues;
     }
-    
-    public Map<String, Integer> getLabelCounts(Map<String, BusinessObjectEntry> businessObjectEntries){
+
+    public Map<String, Integer> getLabelCounts(Map<String, BusinessObjectEntry> businessObjectEntries) {
         Map<String, Integer> labels = new HashMap<String, Integer>();
-        
-        for (String businessObject: businessObjectEntries.keySet()) {
+
+        for (String businessObject : businessObjectEntries.keySet()) {
             org.kuali.rice.kns.datadictionary.BusinessObjectEntry businessObjectEntry = (org.kuali.rice.kns.datadictionary.BusinessObjectEntry) businessObjectEntries.get(businessObject);
-            if ((businessObjectEntry.hasLookupDefinition()) 
-                    && (businessObject.startsWith("org.kuali.kra") 
-                            || businessObject.equals("org.kuali.rice.location.impl.campus.CampusBo")
-                            || businessObject.equals("org.kuali.rice.location.impl.country.CountryBo")
-                            || businessObject.equals("org.kuali.rice.location.impl.county.CountyBo")
-                            || businessObject.equals("org.kuali.rice.location.impl.postalcode.PostalCodeBo")
-                            || businessObject.equals("org.kuali.rice.location.impl.state.StateBo"))) {
-                String label = StringUtils.removeEnd(businessObjectEntry.getLookupDefinition().getTitle().trim()," Lookup");
-                if(labels.containsKey(label)){
+            if ((businessObjectEntry.hasLookupDefinition())
+                    && (businessObject.startsWith("org.kuali.kra") || businessObject.startsWith("org.ariahgroup.research")
+                    || businessObject.equals("org.kuali.rice.location.impl.campus.CampusBo")
+                    || businessObject.equals("org.kuali.rice.location.impl.country.CountryBo")
+                    || businessObject.equals("org.kuali.rice.location.impl.county.CountyBo")
+                    || businessObject.equals("org.kuali.rice.location.impl.postalcode.PostalCodeBo")
+                    || businessObject.equals("org.kuali.rice.location.impl.state.StateBo"))) {
+                String label = StringUtils.removeEnd(businessObjectEntry.getLookupDefinition().getTitle().trim(), " Lookup");
+                if (labels.containsKey(label)) {
                     Integer count = labels.get(label) + 1;
                     labels.put(label, count);
                 } else {
@@ -106,7 +95,7 @@ public class LookupableBoValuesFinder extends UifKeyValuesFinderBase {
                 }
             }
         }
-        
+
         return labels;
     }
 
