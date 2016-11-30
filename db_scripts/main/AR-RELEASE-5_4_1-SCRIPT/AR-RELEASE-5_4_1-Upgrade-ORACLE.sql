2a27dab0d8daf9aa0bebc801a62574d122d5cb28
Ariah Group <code@ariahgroup.org>
2016-11-30 12:58:30 -0500

    RES-753

---
+++ db_scripts/main/AR-RELEASE-5_4_1-SCRIPT/AR-RELEASE-5_4_1-Upgrade-ORACLE.sql
@@ -0,0 +1,9 @@
+set define off
+set sqlblanklines on
+spool AR-RELEASE-5_4_1-Upgrade-ORACLE-Install.log
+
+
+
+
+commit;
+exit
---
+++ db_scripts/main/AR-RELEASE-5_4_1-SCRIPT/KR-RELEASE-5_4_1-Upgrade-ORACLE.sql
@@ -0,0 +1,7 @@
+set define off
+set sqlblanklines on
+spool KR-RELEASE-5_4_1-Upgrade-ORACLE-Install.log
+
+
+commit;
+exit
