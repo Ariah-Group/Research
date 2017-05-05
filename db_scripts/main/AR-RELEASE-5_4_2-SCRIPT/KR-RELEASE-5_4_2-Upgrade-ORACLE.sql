a195dd32b5e2269aa043d63779580de41df98587
Ariah Group <code@ariahgroup.org>
2017-05-04 21:45:34 -0400

    RES-774
    Maintenance - Unable to Edit Maintenance Docs After Viewing in Doc
    Search

--- db_scripts/main/AR-RELEASE-5_4_2-SCRIPT/KR-RELEASE-5_4_2-Upgrade-ORACLE.sql
+++ db_scripts/main/AR-RELEASE-5_4_2-SCRIPT/KR-RELEASE-5_4_2-Upgrade-ORACLE.sql
@@ -2,7 +2,13 @@ set define off
 set sqlblanklines on
 spool KR-RELEASE-5_4_2-Upgrade-ORACLE-Install.log
 
-
+--RES-774
+delete from KRIM_PERM_ATTR_DATA_T where PERM_ID='894' and ATTR_VAL='KC';
+delete from KRIM_PERM_ATTR_DATA_T where PERM_ID='895' and ATTR_VAL='KC';
+delete from KRIM_PERM_ATTR_DATA_T where PERM_ID='896' and ATTR_VAL='KC';
+delete from KRIM_PERM_ATTR_DATA_T where PERM_ID='897' and ATTR_VAL='KC';
+delete from KRIM_PERM_ATTR_DATA_T where PERM_ID='898' and ATTR_VAL='KC';
+delete from KRIM_PERM_ATTR_DATA_T where PERM_ID='899' and ATTR_VAL='KC';
 
 
 commit;
